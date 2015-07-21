91199653cb342306f46a0321a4df1a78ccd41dac
klee <klee@serena.com>
2015-07-21 09:34:35 +0100

    initial commit

---
+++ src/main/java/domain/Thing.java
@@ -0,0 +1,24 @@
+import java.lang.Override;
+import java.lang.String;
+
+public class Thing {
+
+    private String name;
+
+    public Thing(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+    <h2>This is an example Web App</h2>
+</body>
+</html>
